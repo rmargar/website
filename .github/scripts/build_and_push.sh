5f59e0e489296e37e6443721b0d3db1e5d9da950
Ricardo Martinez <ricardomg@riseup.net>
2023-01-24 22:31:19 -0600

    trying to fix gha

--- .github/scripts/build_and_push.sh
+++ .github/scripts/build_and_push.sh
@@ -11,4 +11,3 @@ COMMAND="docker build -t $FULL_IMAGE_NAME -t $IMAGE_NAME_WITH_REGISTRY:latest -f
 /bin/bash -c "$COMMAND"
 docker push $IMAGE_NAME_WITH_REGISTRY:latest
 docker push $FULL_IMAGE_NAME
-rm /tmp/build_args
