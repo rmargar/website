7ac1c7de49a82e03449713b00527353f2358b2aa
Ricardo Martinez <ricardomg@riseup.net>
2023-01-25 13:02:15 -0600

    trying to fix the pipeline

--- .github/scripts/build_and_push.sh
+++ .github/scripts/build_and_push.sh
@@ -7,8 +7,8 @@ rm /tmp/build_args || echo OK
 env >/tmp/build_args
 echo "--build-arg \""$(cat /tmp/build_args | sed -z 's/\n/" --build-arg "/g')"IGNORE_VAR=IGNORE_VAR\"" >/tmp/build_args
 BUILD_ARGS=$(cat /tmp/build_args)
-COMMAND="docker build -t $FULL_IMAGE_NAME -t $IMAGE_NAME_WITH_REGISTRY:latest -f $DOCKERFILE $BUILD_ARGS --no-cache ."
+COMMAND="docker build -t $FULL_IMAGE_NAME -t $IMAGE_NAME:latest -f $DOCKERFILE $BUILD_ARGS --no-cache ."
 /bin/bash -c "$COMMAND"
-echo $IMAGE_NAME_WITH_REGISTRY
-docker push $IMAGE_NAME_WITH_REGISTRY:latest
+echo $IMAGE_NAME
+docker push $IMAGE_NAME:latest
 docker push $FULL_IMAGE_NAME
