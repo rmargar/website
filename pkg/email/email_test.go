489375a2d1382cd52e37765ee15dd5b5e1ec4c6a
Ricardo Martinez <ricardomg@riseup.net>
2023-02-01 12:38:09 -0600

    tests: check if error is logged when email fails

--- pkg/email/email_test.go
+++ pkg/email/email_test.go
@@ -1,12 +1,19 @@
 package email
 
 import (
+	"bytes"
 	"errors"
 	"net/smtp"
+	"os"
+	"strings"
 	"testing"
+
+	log "github.com/sirupsen/logrus"
 )
 
-func TestSendEmailReturnsError(t *testing.T) {
+func TestSendEmail_LogsError(t *testing.T) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
 	mockError := errors.New("Oh oh")
 	send := func(addr string, a smtp.Auth, from string, to []string, msg []byte) error {
 		return mockError
@@ -14,23 +21,23 @@ func TestSendEmailReturnsError(t *testing.T) {
 	cfg := SmtpConfig{}
 	to := []string{"test@host.com"}
 	message := []byte("testMsg")
-	err := SendEmail(&cfg, to, message, send)
-
-	if err != mockError {
-		t.Errorf("expected error (%v), got error (%v)", mockError, err)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+	SendEmail(&cfg, to, message, send)
+	msg := buf.String()
+	t.Log(msg)
+	if !(strings.Contains(msg, "Email couldn't be sent") && strings.Contains(msg, "level=error")) {
+		t.Errorf("SendEmail() did not log error, got: %s", msg)
 	}
 }
 
-func TestSendEmailSuccess(t *testing.T) {
+func TestSendEmail_Success(t *testing.T) {
 	send := func(addr string, a smtp.Auth, from string, to []string, msg []byte) error {
 		return nil
 	}
 	cfg := SmtpConfig{}
 	to := []string{"test@host.com"}
 	message := []byte("testMsg")
-	err := SendEmail(&cfg, to, message, send)
-
-	if err != nil {
-		t.Errorf("expected error (%v), got error (%v)", nil, err)
-	}
+	SendEmail(&cfg, to, message, send)
 }
